cc5898d0c03dd2940fc09f7afb9e8e72ff1cfcee
SametGunduz <sametgunduz@outlook.com>
2018-12-03 20:16:57 +0300

    * Test.csproj: * UnitTest1.cs: * ShoppingCartTests.cs:
    * Trendyol.sln: xunit created

---
+++ Test/ShoppingCartTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Moq;
+using Trendyol.Entities.Concrate;
+using Trendyol.Services.Abstract;
+using Trendyol.Services.Concrate;
+using Xunit;
+
+namespace Test
+{
+    public class ShoppingCartTests
+    {
+		[Fact]
+		public void Shopping_Cart_Add_New_Item()
+		{
+			//Arrange
+            var expectedCount = 1;
+			var mockStrategy = new Mock<IDeliveryStrategy>();
+            var context = new ShoppingCartContext(mockStrategy.Object);
+
+			//Act
+			context.AddItem(new Product("tv", new Category("electronics"), 30, 1));
+
+            //Assert
+            Assert.Equal(expectedCount, context.GetItemCount());
+		}
+    }
+}
